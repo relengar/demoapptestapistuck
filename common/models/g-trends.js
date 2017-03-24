1f0031c5517c0be2751ad23d8e05fc9e5fb45679
relengar <valquendil@gmail.com>
2017-03-24 13:34:27 +0100

    gTrends api first test

---
+++ common/models/g-trends.js
@@ -0,0 +1,38 @@
+'use strict';
+//
+// module.exports = function(Gtrends) {
+//
+// };
+
+
+var request = require("request");
+const googleTrends = require('google-trends-api');
+
+module.exports = function(Gtrends) {
+  Gtrends.find = function(msg, cb) {
+    process.nextTick(function() {
+      msg = msg ? msg : 'ibm';
+      // googleTrends.interestOverTime({keyword: msg})
+      // .then(function(result){
+      //   cb(null, result);
+      // })
+      // .catch(function(err){
+      //   cb(null, err);
+      // });
+      // cb(null, 'Sender says ' + msg + ' to YOU!!!');
+
+      googleTrends.relatedTopics({keyword: msg})
+      .then(function(result){
+        var respData = [];
+        var data = JSON.parse(result).default.rankedList[0].rankedKeyword;
+        for (var i = 0; i < data.length; i++) {
+          respData.push(data[i].topic.type);
+        }
+        cb(null, {"response": result});
+      })
+      .catch(function(err){
+        cb(null, err);
+      });
+    });
+  };
+};
