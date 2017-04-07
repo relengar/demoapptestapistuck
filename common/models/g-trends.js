3d060166cb25e12acc4959254bdfebe61e525129
relengar <valquendil@gmail.com>
2017-04-07 15:00:30 +0200

    successful addition of search parameters via filter query parameter in url

--- common/models/g-trends.js
+++ common/models/g-trends.js
@@ -1,38 +1,56 @@
 'use strict';
-//
-// module.exports = function(Gtrends) {
-//
-// };
 
 
 var request = require("request");
 const googleTrends = require('google-trends-api');
+var loopback = require("loopback");
 
 module.exports = function(Gtrends) {
   Gtrends.find = function(msg, cb) {
-    process.nextTick(function() {
-      msg = msg ? msg : 'ibm';
-      // googleTrends.interestOverTime({keyword: msg})
-      // .then(function(result){
-      //   cb(null, result);
-      // })
-      // .catch(function(err){
-      //   cb(null, err);
-      // });
-      // cb(null, 'Sender says ' + msg + ' to YOU!!!');
-
-      googleTrends.relatedTopics({keyword: msg})
-      .then(function(result){
-        var respData = [];
-        var data = JSON.parse(result).default.rankedList[0].rankedKeyword;
-        for (var i = 0; i < data.length; i++) {
-          respData.push({"description" : data[i].topic.type, "title" : data[i].topic.title, "location" : "None", "link" : data[i].link, "date" : "2012-11-03T07:00:00"});
-        }
-        cb(null, respData);
-      })
-      .catch(function(err){
-        cb(null, err);
-      });
+    // msg = loopback.getCurrentContext();
+    msg = msg ? msg.content : 'ibm';
+    // googleTrends.interestOverTime({keyword: msg})
+    // .then(function(result){
+    //   cb(null, result);
+    // })
+    // .catch(function(err){
+    //   cb(null, err);
+    // });
+    // cb(null, "the message is " + msg);
+    googleTrends.relatedTopics({keyword: msg})
+    .then(function(result){
+      var respData = [];
+      var data = JSON.parse(result).default.rankedList[0].rankedKeyword;
+      for (var i = 0; i < data.length; i++) {
+        respData.push({"description" : data[i].topic.type, "title" : data[i].topic.title, "location" : "None", "link" : data[i].link, "date" : "2012-11-03T07:00:00"});
+      }
+      cb(null, respData);
+    })
+    .catch(function(err){
+      cb(null, err);
     });
   };
+
+  Gtrends.findOne = function(param, cb) {
+    cb(null, "findOne" + param);
+  };
+
+  Gtrends.remoteMethod('find', {
+    http: {path: '/Gtrends', verb: 'get'},
+    accepts: {arg: 'msg', type: 'string'},
+    returns: {arg: 'greeting', type: 'string'}
+  });
+
+  // Gtrends.setup = function() {
+  //   Gtrends.base.setup.apply(this, arguments);
+  //
+  //   this.remoteMethod('nearby', {
+  //     description: 'Find nearby locations around the geo point',
+  //     accepts: [{arg: 'msg', type: 'string'}],
+  //     returns: {arg: 'greeting', type: 'string'},
+  //     http: { verb: 'GET' }
+  //   });
+  // };
+  //
+  // Gtrends.setup();
 };
